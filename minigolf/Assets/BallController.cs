2093eaacba5de0af923dca27c1d238bbbeca6b46
fanura <81693732+fanura@users.noreply.github.com>
2023-05-15 21:44:18 +0700

    yang kedua

---
+++ minigolf/Assets/BallController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] Collider col;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float force;
+
+    bool shoot;
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out var hitInfo))
+            {
+                if(hitInfo.Collider == col)
+            shoot = true;
+            }
+        }
+    }
+    private void FixedUpdate() {
+        if(shoot)
+        {
+            shoot = false;
+            Vector3 direction = Camera.main.transform.forward;
+            direction.y = 0;
+            rb.AddForce(direction * force, ForceMode.Impulse);
+        }
+        
+        if(rb.velocity.sqrMagnitude < 0.01f && rb.velocity.sqrMagnitude > 0)
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    public bool IsMove()
+    {
+        return rb.velocity != Vector3.zero;
+    }
+}
---
+++ minigolf/Assets/PlayManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayManager : MonoBehaviour
+{
+    [SerializeField] BallController ballController;
+    [SerializeField] CameraController camController;
+
+    private void Update(){
+        var inputActive = Input.GetMouseButton(0) && ballController.IsMove() == false;
+        camController.SetInputActive(Input.GetMouseButton(0));
+    }
+
+}
